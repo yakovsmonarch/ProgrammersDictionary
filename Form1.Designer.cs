4a7d5f6f7a6d2e191ab36c04fa33e9f065abc366
yakovsmonarch <medvejonok07@gmail.com>
2022-04-06 15:46:20 +0800

    Create project

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace ProgrammersDictionary
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WordslistBox = new System.Windows.Forms.ListBox();
+            this.AddWordTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WordslistBox
+            // 
+            this.WordslistBox.FormattingEnabled = true;
+            this.WordslistBox.ItemHeight = 16;
+            this.WordslistBox.Location = new System.Drawing.Point(12, 107);
+            this.WordslistBox.Name = "WordslistBox";
+            this.WordslistBox.Size = new System.Drawing.Size(496, 548);
+            this.WordslistBox.TabIndex = 0;
+            // 
+            // AddWordTextBox
+            // 
+            this.AddWordTextBox.Location = new System.Drawing.Point(12, 67);
+            this.AddWordTextBox.Name = "AddWordTextBox";
+            this.AddWordTextBox.Size = new System.Drawing.Size(397, 22);
+            this.AddWordTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(425, 66);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(83, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 667);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.AddWordTextBox);
+            this.Controls.Add(this.WordslistBox);
+            this.Name = "MainForm";
+            this.Text = "Словарь программиста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox WordslistBox;
+        private System.Windows.Forms.TextBox AddWordTextBox;
+        private System.Windows.Forms.Button AddButton;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ProgrammersDictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProgrammersDictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1ccd6668-861d-43ef-adf7-fbe3ecd4fb58")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
